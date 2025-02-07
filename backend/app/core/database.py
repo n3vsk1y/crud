66ac01e1facf12622454592a77757751d7f51737
n3vsk1y <alexafanasenko900@gmail.com>
2025-02-08 01:05:44 +0300

    docker

---
+++ backend/app/core/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from app.core.config import settings
+
+engine = create_async_engine(settings.DATABASE_URL_asyncpg, echo=False)
+
+SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
+Base = declarative_base()
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
+
+
---
+++ backend/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.core.database import Base, engine
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
+
+@app.get("/")
+def read_root():
+    return {"App": "Started"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
